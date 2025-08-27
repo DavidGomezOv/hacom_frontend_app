71400993800efeab763732f94a0df1aa7d2b738e
DavidGomezOv <david182400@gmail.com>
2025-08-26 19:41:48 -0600

    Created base screen widget

--- lib/features/dashboard/presentation/dashboard_page.dart
+++ lib/features/dashboard/presentation/dashboard_page.dart
@@ -4,64 +4,45 @@ import 'package:go_router/go_router.dart';
 import 'package:hacom_frontend_app/core/router/app_router.dart';
 import 'package:hacom_frontend_app/features/auth/presentation/bloc/auth_cubit.dart';
 import 'package:hacom_frontend_app/features/dashboard/presentation/widgets/dashboard_item_button_widget.dart';
+import 'package:hacom_frontend_app/shared/widgets/base_page.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AuthCubit, AuthState>(
-      listener: (context, state) {
-        state.whenOrNull(logout: () => context.goNamed(AppRouter.loginRouteName));
-      },
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 20,
-              children: [
-                Text(
-                  'Welcome!',
-                  style: Theme.of(
-                    context,
-                  ).textTheme.headlineLarge?.copyWith(fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'Please select what you want to do today.',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                SizedBox(height: 30),
-                DashboardItemButtonWidget(
-                  buttonTitle: 'Supervisor',
-                  buttonDescription: 'This is a short description of the supervisor menu.',
-                  onPressed: () {
-                    context.goNamed(AppRouter.supervisorRouteName);
-                  },
-                ),
-                DashboardItemButtonWidget(
-                  buttonTitle: 'Notifications',
-                  buttonDescription: 'This is a short description of the supervisor menu.',
-                  onPressed: () {},
-                ),
-                DashboardItemButtonWidget(
-                  buttonTitle: 'Places',
-                  buttonDescription: 'This is a short description of the supervisor menu.',
-                  onPressed: () {},
-                ),
-                Spacer(),
-                DashboardItemButtonWidget(
-                  buttonTitle: 'Logout',
-                  onPressed: () {
-                    context.read<AuthCubit>().logout();
-                  },
-                ),
-              ],
-            ),
+    return BasePage(
+      pageTitle: 'Welcome!',
+      pageDescription: 'Please select what you want to do today.',
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 20,
+        children: [
+          DashboardItemButtonWidget(
+            buttonTitle: 'Supervisor',
+            buttonDescription: 'This is a short description of the supervisor menu.',
+            onPressed: () {
+              context.goNamed(AppRouter.supervisorRouteName);
+            },
           ),
-        ),
+          DashboardItemButtonWidget(
+            buttonTitle: 'Notifications',
+            buttonDescription: 'This is a short description of the supervisor menu.',
+            onPressed: () {},
+          ),
+          DashboardItemButtonWidget(
+            buttonTitle: 'Places',
+            buttonDescription: 'This is a short description of the supervisor menu.',
+            onPressed: () {},
+          ),
+          Spacer(),
+          DashboardItemButtonWidget(
+            buttonTitle: 'Logout',
+            onPressed: () {
+              context.read<AuthCubit>().logout();
+            },
+          ),
+        ],
       ),
     );
   }
---
+++ lib/shared/widgets/base_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BasePage extends StatelessWidget {
+  const BasePage({
+    super.key,
+    required this.pageTitle,
+    this.pageDescription,
+    this.showBackButton = false,
+    this.leading,
+    required this.content,
+  });
+
+  final String pageTitle;
+  final String? pageDescription;
+  final bool showBackButton;
+  final Widget? leading;
+  final Widget content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (showBackButton)
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios_new_rounded),
+                      visualDensity: VisualDensity.compact,
+                      onPressed: () => context.pop(),
+                    ),
+                  Text(
+                    'Supervisor',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.headlineLarge?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  if (leading != null) leading!,
+                ],
+              ),
+              Text(
+                'Short description of supervisor module.',
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 20),
+              ),
+              SizedBox(height: 30),
+              Flexible(child: content),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
